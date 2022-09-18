ed67551164c53e270cc4309670c8ab29540c2ccd
55nknown <55922348+55nknown@users.noreply.github.com>
2022-09-18 18:40:40 +0200

    foreground_service setup

--- filcnaplo/macos/Flutter/GeneratedPluginRegistrant.swift
+++ filcnaplo/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,8 +8,10 @@ import Foundation
 import connectivity_plus_macos
 import dynamic_color
 import flutter_acrylic
+import flutter_local_notifications
 import path_provider_macos
 import share_plus_macos
+import shared_preferences_macos
 import sqflite
 import url_launcher_macos
 
@@ -17,8 +19,10 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   DynamicColorPlugin.register(with: registry.registrar(forPlugin: "DynamicColorPlugin"))
   FlutterAcrylicPlugin.register(with: registry.registrar(forPlugin: "FlutterAcrylicPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
