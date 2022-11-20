f38ff3b862868f3ccfefcc82a9028c8d53194b9f
55nknown <annon@annon.me>
2022-11-20 23:03:50 +0100

    fix build

--- filcnaplo/build.sh
+++ filcnaplo/build.sh
@@ -9,12 +9,5 @@ function get_version
   cat pubspec.yaml | grep version: | cut -d' ' -f2 | cut -d+ -f1
 end
 
-set DEFAULT_KEY $HOME/keys/filc3.properties
-
-if test -e $DEFAULT_KEY
-  echo Using \"$DEFAULT_KEY\" for signing
-  set -x ANDROID_SIGNING $DEFAULT_KEY
-end
-
-flutter build apk --release --dart-define=APPVER=(get_version) --no-tree-shake-icons
-cp -v "build/app/outputs/flutter-apk/app-release.apk" ~/"Desktop/hu.filc.naplo_"(get_version_bn).apk
+flutter build apk --release --dart-define=APPVER=(get_version) --no-tree-shake-icons && \
+    cp -v "build/app/outputs/flutter-apk/app-release.apk" ~/"Desktop/hu.filc.naplo_"(get_version_bn).apk
