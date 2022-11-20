97b01e9f1459e9570e7f6d6ca265237d582bc062
55nknown <annon@annon.me>
2022-11-20 22:03:46 +0100

    fix build script

--- filcnaplo/build.sh
+++ filcnaplo/build.sh
@@ -1,4 +1,4 @@
-#!/bin/fish
+#!/usr/bin/env fish
 
 # With build number
 function get_version_bn
@@ -9,11 +9,9 @@ function get_version
   cat pubspec.yaml | grep version: | cut -d' ' -f2 | cut -d+ -f1
 end
 
-if test -e /mnt/enc/keys/filc3.properties
-  set -x ANDROID_SIGNING /mnt/enc/keys/filc3.properties
+if test -e ~/keys/filc3.properties
+  set -x ANDROID_SIGNING ~/keys/filc3.properties
 end
 
 flutter build apk --release --dart-define=APPVER=(get_version) --no-tree-shake-icons
 cp -v "build/app/outputs/flutter-apk/app-release.apk" ~/"Desktop/hu.filc.naplo_"(get_version_bn).apk
-
-notify-send "Flutter" "Apk build done."
