36bd679644b66d0d691b27679414459b8ff300fc
55nknown <annon@annon.me>
2022-11-20 19:27:06 +0100

    cleanup

--- filcnaplo/lib/database/store.dart
+++ filcnaplo/lib/database/store.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'dart:developer';
 import 'package:filcnaplo/models/subject_lesson_count.dart';
 // ignore: depend_on_referenced_packages
 import 'package:sqflite_common/sqlite_api.dart';
