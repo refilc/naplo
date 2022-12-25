1379a059a9af887e426f563ae85d756340b4df5c
55nknown <annon@annon.me>
2022-12-25 14:44:02 +0100

    fix icons not showing

--- filcnaplo/lib/icons/filc_icons.dart
+++ filcnaplo/lib/icons/filc_icons.dart
@@ -6,38 +6,38 @@ class FilcIcons {
   static const iconFontFamily = 'FilcIcons';
 
   /// home
-  static const IconData home = IconData(0x00, fontFamily: iconFontFamily);
+  static const IconData home = IconData(0x21, fontFamily: iconFontFamily);
 
   /// linux
-  static const IconData linux = IconData(0x01, fontFamily: iconFontFamily);
+  static const IconData linux = IconData(0x22, fontFamily: iconFontFamily);
 
   /// upstairs
-  static const IconData upstairs = IconData(0x02, fontFamily: iconFontFamily);
+  static const IconData upstairs = IconData(0x23, fontFamily: iconFontFamily);
 
   /// downstairs
-  static const IconData downstairs = IconData(0x03, fontFamily: iconFontFamily);
+  static const IconData downstairs = IconData(0x24, fontFamily: iconFontFamily);
 
   /// premium
-  static const IconData premium = IconData(0x04, fontFamily: iconFontFamily);
+  static const IconData premium = IconData(0x25, fontFamily: iconFontFamily);
 
   /// tinta
-  static const IconData tinta = IconData(0x05, fontFamily: iconFontFamily);
+  static const IconData tinta = IconData(0x26, fontFamily: iconFontFamily);
 
   /// kupak
-  static const IconData kupak = IconData(0x06, fontFamily: iconFontFamily);
+  static const IconData kupak = IconData(0x27, fontFamily: iconFontFamily);
 
   /// homefill
-  static const IconData homefill = IconData(0x07, fontFamily: iconFontFamily);
+  static const IconData homefill = IconData(0x28, fontFamily: iconFontFamily);
 
   /// gradesfill
-  static const IconData gradesfill = IconData(0x08, fontFamily: iconFontFamily);
+  static const IconData gradesfill = IconData(0x29, fontFamily: iconFontFamily);
 
   /// timetablefill
-  static const IconData timetablefill = IconData(0x09, fontFamily: iconFontFamily);
+  static const IconData timetablefill = IconData(0x2a, fontFamily: iconFontFamily);
 
   /// messagesfill
-  static const IconData messagesfill = IconData(0x0a, fontFamily: iconFontFamily);
+  static const IconData messagesfill = IconData(0x2b, fontFamily: iconFontFamily);
 
   /// absencesfill
-  static const IconData absencesfill = IconData(0x0b, fontFamily: iconFontFamily);
+  static const IconData absencesfill = IconData(0x2c, fontFamily: iconFontFamily);
 }
