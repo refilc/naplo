03a779ea9cb8daada8291fb90ea70469c979af61
Móricz Gergő <mo.geryy@gmail.com>
2023-05-27 15:45:55 +0200

    fix(build): scripts shouldn't need fish

--- filcnaplo/build-ipa.sh
+++ filcnaplo/build-ipa.sh
@@ -1,12 +1,3 @@
-#!/usr/bin/env fish
+#!/bin/sh
 
-# With build number
-function get_version_bn
-  cat pubspec.yaml | grep version: | cut -d' ' -f2
-end
-
-function get_version
-  cat pubspec.yaml | grep version: | cut -d' ' -f2 | cut -d+ -f1
-end
-
-flutter build ipa --release --dart-define=APPVER=(get_version) --no-tree-shake-icons
+flutter build ipa --release --dart-define=APPVER=$(cat pubspec.yaml | grep version: | cut -d' ' -f2 | cut -d+ -f1) --no-tree-shake-icons
--- filcnaplo/build.sh
+++ filcnaplo/build.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env sh
+#!/bin/sh
 
 flutter build apk --release --dart-define=APPVER=$(cat pubspec.yaml | grep version: | cut -d' ' -f2 | cut -d+ -f1) --no-tree-shake-icons
