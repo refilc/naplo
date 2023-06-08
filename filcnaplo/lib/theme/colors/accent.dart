8dbf605450aff56a3087e9d305265ee4dc313d07
Kima <kimavideos97@gmail.com>
2023-06-08 21:28:28 +0200

    added black accent color

--- filcnaplo/lib/theme/colors/accent.dart
+++ filcnaplo/lib/theme/colors/accent.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 enum AccentColor {
-  ogfilc,
   filc,
   blue,
   green,
@@ -11,12 +10,13 @@ enum AccentColor {
   red,
   pink,
   purple,
+  none,
+  ogfilc,
   adaptive,
   custom
 }
 
 Map<AccentColor, Color> accentColorMap = {
-  AccentColor.ogfilc: const Color(0xff20AC9B),
   AccentColor.filc: const Color(0xFF3D7BF4),
   AccentColor.blue: Colors.blue.shade300,
   AccentColor.green: Colors.green.shade400,
@@ -26,6 +26,8 @@ Map<AccentColor, Color> accentColorMap = {
   AccentColor.red: Colors.red.shade300,
   AccentColor.pink: Colors.pink.shade300,
   AccentColor.purple: Colors.purple.shade300,
+  //AccentColor.none: Colors.black,
+  AccentColor.ogfilc: const Color(0xff20AC9B),
   AccentColor.adaptive: const Color(0xFF3D7BF4),
   AccentColor.custom: const Color(0xFF3D7BF4),
 };
