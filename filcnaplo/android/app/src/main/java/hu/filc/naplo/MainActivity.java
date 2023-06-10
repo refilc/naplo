95fa819ed2d86e8655bf9a9b806571f147952e3c
Kima <kimavideos97@gmail.com>
2023-06-10 13:26:28 +0200

    updatet things in pubspec

--- filcnaplo/android/app/src/main/java/hu/filc/naplo/MainActivity.java
+++ filcnaplo/android/app/src/main/java/hu/filc/naplo/MainActivity.java
@@ -1,18 +1,9 @@
 package hu.refilc.naplo;
 
-import android.os.Bundle;
-
 import io.flutter.embedding.android.FlutterActivity;
-import io.flutter.plugins.GeneratedPluginRegistrant;
 
 import io.flutter.embedding.engine.FlutterEngine;
 
 public class MainActivity extends FlutterActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        GeneratedPluginRegistrant.registerWith(new FlutterEngine(this));
-    }
+    
 }
--- filcnaplo/macos/Flutter/GeneratedPluginRegistrant.swift
+++ filcnaplo/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,19 +7,21 @@ import Foundation
 
 import connectivity_plus
 import dynamic_color
-import flutter_acrylic
+import file_selector_macos
 import flutter_local_notifications
+import macos_window_utils
 import package_info_plus
 import path_provider_foundation
-import share_plus_macos
+import share_plus
 import sqflite
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   DynamicColorPlugin.register(with: registry.registrar(forPlugin: "DynamicColorPlugin"))
-  FlutterAcrylicPlugin.register(with: registry.registrar(forPlugin: "FlutterAcrylicPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+  MacOSWindowUtilsPlugin.register(with: registry.registrar(forPlugin: "MacOSWindowUtilsPlugin"))
   FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
