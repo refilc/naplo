c60743114cc3860f6f9f3694120a4ed026fc5e35
Kima <kimavideos97@gmail.com>
2023-09-10 19:03:34 +0200

    finished new style message view

--- filcnaplo_mobile_ui/lib/common/widgets/message/message_view_tile.dart
+++ filcnaplo_mobile_ui/lib/common/widgets/message/message_view_tile.dart
@@ -1,16 +1,18 @@
 import 'package:filcnaplo/api/providers/user_provider.dart';
-import 'package:filcnaplo/utils/color.dart';
+import 'package:filcnaplo/theme/colors/colors.dart';
 import 'package:filcnaplo_kreta_api/models/message.dart';
 import 'package:filcnaplo_mobile_ui/common/panel/panel.dart';
-import 'package:filcnaplo_mobile_ui/common/profile_image/profile_image.dart';
+import 'package:filcnaplo_mobile_ui/common/profile_image/character_image.dart';
 import 'package:filcnaplo_mobile_ui/common/widgets/message/attachment_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:filcnaplo/utils/format.dart';
 import 'package:flutter_custom_tabs/flutter_custom_tabs.dart';
+import 'package:flutter_feather_icons/flutter_feather_icons.dart';
 // import 'package:flutter_feather_icons/flutter_feather_icons.dart';
 import 'package:flutter_linkify/flutter_linkify.dart';
 import 'package:provider/provider.dart';
 import 'message_view_tile.i18n.dart';
+import 'package:share_plus/share_plus.dart';
 
 class MessageViewTile extends StatelessWidget {
   const MessageViewTile(this.message, {Key? key}) : super(key: key);
@@ -50,59 +52,110 @@ class MessageViewTile extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          // Subject
-          Text(
-            message.subject,
-            softWrap: true,
-            maxLines: 10,
-            style: const TextStyle(
-              fontWeight: FontWeight.w600,
-              fontSize: 24.0,
+          // subject
+          Center(
+            child: Text(
+              message.subject,
+              softWrap: true,
+              maxLines: 10,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: 24.0,
+                height: 1.2,
+                color: Theme.of(context).textTheme.bodySmall?.color,
+              ),
             ),
           ),
+          const SizedBox(height: 8.0),
 
-          // Author
-          ListTile(
-            visualDensity: VisualDensity.compact,
-            contentPadding: EdgeInsets.zero,
-            leading: ProfileImage(
-              name: message.author,
-              backgroundColor: ColorUtils.stringToColor(message.author),
-            ),
-            title: Text(
-              message.author,
-              style: const TextStyle(fontWeight: FontWeight.w600),
-              overflow: TextOverflow.ellipsis,
-              maxLines: 2,
-            ),
-            subtitle: Text(
-              "to".i18n + " " + recipientLabel,
-              style: const TextStyle(fontWeight: FontWeight.w500),
-              overflow: TextOverflow.ellipsis,
-              maxLines: 1,
-            ),
-            trailing: const Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                // IconButton(
-                //   onPressed: () {},
-                //   icon: Icon(FeatherIcons.cornerUpLeft, color: AppColors.of(context).text),
-                //   splashRadius: 24.0,
-                //   padding: EdgeInsets.zero,
-                //   visualDensity: VisualDensity.compact,
-                // ),
-                // IconButton(
-                //   onPressed: () {},
-                //   icon: Icon(FeatherIcons.share2, color: AppColors.of(context).text),
-                //   splashRadius: 24.0,
-                //   padding: EdgeInsets.zero,
-                //   visualDensity: VisualDensity.compact,
-                // ),
-              ],
+          // date
+          Center(
+            child: Text(
+              message.date.format(context),
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontWeight: FontWeight.w500,
+                color: Theme.of(context).textTheme.bodySmall?.color,
+              ),
             ),
           ),
+          const SizedBox(height: 28.0),
 
-          // Content
+          // author
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16.0),
+              border: Border.all(
+                color: Theme.of(context).colorScheme.outline,
+                width: 1.0,
+              ),
+            ),
+            child: ListTile(
+              visualDensity: VisualDensity.compact,
+              contentPadding: EdgeInsets.zero,
+              leading: CharacterImage(
+                name: message.author,
+                backgroundColor: Theme.of(context).colorScheme.secondary,
+                radius: 22.0,
+              ),
+              title: Text(
+                message.author,
+                style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  color: Theme.of(context).textTheme.bodySmall?.color,
+                ),
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+              ),
+              subtitle: Text(
+                "to".i18n + " " + recipientLabel,
+                style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  height: 1.2,
+                  color: Theme.of(context)
+                      .textTheme
+                      .bodySmall
+                      ?.color
+                      ?.withOpacity(0.6),
+                ),
+                overflow: TextOverflow.ellipsis,
+                maxLines: 1,
+              ),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  // IconButton(
+                  //   onPressed: () {},
+                  //   icon: Icon(FeatherIcons.cornerUpLeft,
+                  //       color: AppColors.of(context).text),
+                  //   splashRadius: 24.0,
+                  //   highlightColor: Colors.transparent,
+                  //   padding: EdgeInsets.zero,
+                  //   visualDensity: VisualDensity.compact,
+                  // ),
+                  IconButton(
+                    onPressed: () {
+                      Share.share(
+                        message.content.escapeHtml(),
+                        subject: 'reFilc',
+                      );
+                    },
+                    icon: Icon(FeatherIcons.share2,
+                        color: AppColors.of(context).text),
+                    splashRadius: 24.0,
+                    highlightColor: Colors.transparent,
+                    padding: EdgeInsets.zero,
+                    visualDensity: VisualDensity.compact,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const SizedBox(height: 10.0),
+
+          // content
           Panel(
             padding: const EdgeInsets.all(12.0),
             child: SelectableLinkify(
