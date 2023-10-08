fd80aec51f38d5e903602b48a8537ecdc0c66323
Kima <kimavideos97@gmail.com>
2023-10-08 16:24:59 +0200

    added upgrade-pub.sh to upgrade pub packages

---
+++ upgrade-pub.sh
@@ -0,0 +1,7 @@
+cd filcnaplo && flutter pub upgrade && cd ..
+cd filcnaplo_kreta_api && flutter pub upgrade && cd ..
+cd filcnaplo_mobile_ui && flutter pub upgrade && cd ..
+cd filcnaplo_desktop_ui && flutter pub upgrade && cd ..
+cd filcnaplo_premium && flutter pub upgrade && cd ..
+
+echo Upgraded pub.
