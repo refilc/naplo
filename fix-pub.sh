e3532c5787340401e5acabaadaa7faef4faa60c5
Kima <kimavideos97@gmail.com>
2024-02-11 21:18:30 +0100

    changed premium path in fix pub

--- fix-pub.sh
+++ fix-pub.sh
@@ -2,6 +2,6 @@ cd filcnaplo && flutter clean && flutter pub get && cd ..
 cd filcnaplo_kreta_api && flutter clean && flutter pub get && cd ..
 cd filcnaplo_mobile_ui && flutter clean && flutter pub get && cd ..
 cd filcnaplo_desktop_ui && flutter clean && flutter pub get && cd ..
-cd filcnaplo_premium && flutter clean && flutter pub get && cd ..
+cd refilc_plus && flutter clean && flutter pub get && cd ..
 
 echo Fixed pub.
