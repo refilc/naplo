c9362dcb8b2879e1d0ec09e056d0a601deb542b9
Kima <kimavideos97@gmail.com>
2024-02-18 17:51:18 +0100

    yas

--- filcnaplo_mobile_ui/lib/pages/grades/grades_count.dart
+++ filcnaplo_mobile_ui/lib/pages/grades/grades_count.dart
@@ -19,37 +19,38 @@ class GradesCount extends StatelessWidget {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text.rich(
-              TextSpan(children: [
-                TextSpan(
-                  text: gradesCount.reduce((a, b) => a + b).toString(),
-                  style: const TextStyle(fontWeight: FontWeight.w600),
-                ),
-                const TextSpan(
-                  text: "x",
-                  style: TextStyle(
-                    fontSize: 13.0,
-                  ),
-                ),
-              ]),
-              style: const TextStyle(fontSize: 15.0),
-            ),
-            const SizedBox(
-              width: 10.0,
-            ),
-            ClipRRect(
-              borderRadius: BorderRadius.circular(10.0),
-              child: VerticalDivider(
-                width: 2,
-                thickness: 2,
-                indent: 2,
-                endIndent: 2,
-                color: MediaQuery.of(context).platformBrightness ==
-                        Brightness.light
-                    ? Colors.grey.shade300
-                    : Colors.grey.shade700,
-              ),
-            ),
+            // TODO: make a new widget here, cuz this will not fit
+            // Text.rich(
+            //   TextSpan(children: [
+            //     TextSpan(
+            //       text: gradesCount.reduce((a, b) => a + b).toString(),
+            //       style: const TextStyle(fontWeight: FontWeight.w600),
+            //     ),
+            //     const TextSpan(
+            //       text: "x",
+            //       style: TextStyle(
+            //         fontSize: 13.0,
+            //       ),
+            //     ),
+            //   ]),
+            //   style: const TextStyle(fontSize: 15.0),
+            // ),
+            // const SizedBox(
+            //   width: 10.0,
+            // ),
+            // ClipRRect(
+            //   borderRadius: BorderRadius.circular(10.0),
+            //   child: VerticalDivider(
+            //     width: 2,
+            //     thickness: 2,
+            //     indent: 2,
+            //     endIndent: 2,
+            //     color: MediaQuery.of(context).platformBrightness ==
+            //             Brightness.light
+            //         ? Colors.grey.shade300
+            //         : Colors.grey.shade700,
+            //   ),
+            // ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: gradesCount
@@ -57,6 +58,9 @@ class GradesCount extends StatelessWidget {
                       padding: const EdgeInsets.only(left: 9.69),
                       child: GradesCountItem(count: e, value: index + 1)))
                   .toList(),
+            ),
+            const SizedBox(
+              width: 8.0,
             ),
           ],
         ),
