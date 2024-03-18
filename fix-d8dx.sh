52530197c3a52dc6a70f7ffb875748073a565c57
ReinerRego <regokoppany@gmail.com>
2024-03-18 21:35:09 +0100

    delete old sh scripts (welcome to contribution windows users)

--- fix-d8dx.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cd $ANDROID_SDK/build-tools/31.0.0 &&
-  mv -v d8 dx &&
-  cd lib &&
-  mv -v d8.jar dx.jar
--- fix-pub.sh
+++
@@ -1,7 +0,0 @@
-cd refilc && flutter clean && flutter pub get && cd ..
-cd refilc_kreta_api && flutter clean && flutter pub get && cd ..
-cd refilc_mobile_ui && flutter clean && flutter pub get && cd ..
-cd refilc_desktop_ui && flutter clean && flutter pub get && cd ..
-cd refilc_plus && flutter clean && flutter pub get && cd ..
-
-echo Fixed pub.
