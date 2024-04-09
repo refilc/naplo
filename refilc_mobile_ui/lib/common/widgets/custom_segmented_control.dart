6f694e8abe056b40d60584e91a3cbc6c8417754e
Kima <kimavideos97@gmail.com>
2024-04-09 22:34:35 +0200

    fixed segmented control color issue

--- refilc_mobile_ui/lib/common/widgets/custom_segmented_control.dart
+++ refilc_mobile_ui/lib/common/widgets/custom_segmented_control.dart
@@ -38,7 +38,7 @@ class CustomSegmentedControl extends StatelessWidget {
         borderRadius: BorderRadius.circular(12.0),
       ),
       thumbDecoration: BoxDecoration(
-        color: Theme.of(context).highlightColor,
+        color: Theme.of(context).colorScheme.background,
         borderRadius: BorderRadius.circular(10.0),
         // boxShadow: [
         //   BoxShadow(
