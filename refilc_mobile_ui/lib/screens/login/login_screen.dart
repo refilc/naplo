d70f92d4e52c69ce916be56dc154dff2553b383d
Kima <kimavideos97@gmail.com>
2024-08-14 12:32:32 +0200

    changed login button

--- refilc_mobile_ui/lib/screens/login/login_screen.dart
+++ refilc_mobile_ui/lib/screens/login/login_screen.dart
@@ -218,42 +218,78 @@ class LoginScreenState extends State<LoginScreen> {
                                 child: Padding(
                                   padding: const EdgeInsets.symmetric(
                                       horizontal: 16),
-                                  child: FilledButton(
-                                      style: ButtonStyle(
-                                          shape: WidgetStateProperty.all<
-                                                  RoundedRectangleBorder>(
-                                              const RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.all(
-                                            Radius.circular(12)),
-                                      ))),
-                                      onPressed: () {
-                                        final NavigatorState navigator =
-                                            Navigator.of(context);
-                                        navigator
-                                            .push(
-                                          MaterialPageRoute(
-                                            builder: (context) =>
-                                                KretenLoginScreen(
-                                              onLogin: (String code) {
-                                                codeController.text = code;
-                                                navigator.pop();
-                                              },
-                                            ),
+                                  child: GestureDetector(
+                                    onTap: () {
+                                      final NavigatorState navigator =
+                                          Navigator.of(context);
+                                      navigator
+                                          .push(
+                                        MaterialPageRoute(
+                                          builder: (context) =>
+                                              KretenLoginScreen(
+                                            onLogin: (String code) {
+                                              codeController.text = code;
+                                              navigator.pop();
+                                            },
                                           ),
-                                        )
-                                            .then((value) {
-                                          if (codeController.text != "") {
-                                            _NewLoginAPI(context: context);
-                                          }
-                                        });
-                                      },
-                                      child: Text(
-                                        "login".i18n,
-                                        style: const TextStyle(
-                                            fontFamily: 'Montserrat',
-                                            fontSize: 20,
-                                            fontWeight: FontWeight.w700),
-                                      )),
+                                        ),
+                                      )
+                                          .then((value) {
+                                        if (codeController.text != "") {
+                                          _NewLoginAPI(context: context);
+                                        }
+                                      });
+                                    },
+                                    child: Container(
+                                        width:
+                                            MediaQuery.of(context).size.width *
+                                                0.75,
+                                        height: 50.0,
+                                        decoration: BoxDecoration(
+                                          // image: const DecorationImage(
+                                          //   image:
+                                          //       AssetImage('assets/images/btn_kreten_login.png'),
+                                          //   fit: BoxFit.scaleDown,
+                                          // ),
+                                          borderRadius:
+                                              BorderRadius.circular(12.0),
+                                          color: const Color(0xFF0097C1),
+                                        ),
+                                        padding: const EdgeInsets.only(
+                                            top: 5.0,
+                                            left: 5.0,
+                                            right: 5.0,
+                                            bottom: 5.0),
+                                        child: Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: [
+                                            Image.asset(
+                                              'assets/images/btn_kreten_login.png',
+                                            ),
+                                            const SizedBox(
+                                              width: 10.0,
+                                            ),
+                                            Container(
+                                              width: 1.0,
+                                              height: 30.0,
+                                              color: Colors.white,
+                                            ),
+                                            const SizedBox(
+                                              width: 10.0,
+                                            ),
+                                            Text(
+                                              'login_w_kreta_acc'.i18n,
+                                              textAlign: TextAlign.center,
+                                              style: const TextStyle(
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 15.0,
+                                              ),
+                                            ),
+                                          ],
+                                        )),
+                                  ),
                                 ),
                               ),
                               const SizedBox(height: 8),
