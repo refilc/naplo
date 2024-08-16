52b9b4f5db9f4b84c57f13c7d3484eba8b04e370
Kima <kimavideos97@gmail.com>
2024-08-16 14:40:48 +0200

    maybe fixed ios login

--- refilc_mobile_ui/lib/screens/login/kreten_login.dart
+++ refilc_mobile_ui/lib/screens/login/kreten_login.dart
@@ -24,6 +24,14 @@ class _KretenLoginScreenState extends State<KretenLoginScreen> {
     controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
       ..setNavigationDelegate(NavigationDelegate(
+        onNavigationRequest: (n) async {
+          if (n.url.startsWith('https://mobil.e-kreta.hu') ||
+              n.url.startsWith('https://idp.e-kreta.hu')) {
+            return NavigationDecision.navigate;
+          } else {
+            return NavigationDecision.prevent;
+          }
+        },
         onPageStarted: (url) async {
           setState(() {
             loadingPercentage = 0;
